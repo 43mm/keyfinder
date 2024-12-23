bb338032d03bcf07db20c9054f76e389b362079a
43mm <18689574+43mm@users.noreply.github.com>
2024-12-23 14:34:24 +1100

    feat: add f keys to layout

--- src/App.svelte
+++ src/App.svelte
@@ -11,13 +11,17 @@
     {#each keyboardRows as row}
       <div class="flex justify-between gap-x-1">
         {#each row as key}
-          <div
-            class="p-4 bg-gray-100 rounded text-center first:text-left last:text-right text-s cursor-pointer hover:outline"
-            style="width: {key.length * 70}px;"
-            id={key.id}
-          >
-            {key.name}
-          </div>
+          {#if key.name == ""}
+            <div class="" style="width: {key.length * 70}px;"></div>
+          {:else}
+            <div
+              class="p-4 bg-gray-100 rounded text-center first:text-left last:text-right text-s cursor-pointer hover:outline-2"
+              style="width: {key.length * 70}px;"
+              id={key.id}
+            >
+              {key.name}
+            </div>
+          {/if}
         {/each}
       </div>
     {/each}
