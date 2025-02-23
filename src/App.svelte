a232f7f286d3a388105e1260f9e885c032890c91
43mm <18689574+43mm@users.noreply.github.com>
2025-02-23 18:51:38 +1100

    feat: add editable keyboard name

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Tooltip } from "bits-ui";
   import Keyboard from "./Keyboard.svelte";
   import KeyboardList from "./KeyboardList.svelte";
+  import { keyboards } from "./keyboards.svelte";
   import "./app.css";
 </script>
 
@@ -9,7 +10,16 @@
   <Tooltip.Provider delayDuration={0}>
     <div class="inline-flex flex-col gap-y-1">
       <div class="flex items-end justify-between">
-        <h1 class="text-4xl">keyfinder</h1>
+        <h1
+          bind:innerHTML={keyboards[0].name}
+          class="text-4xl outline-0"
+          on:keydown={(e) => {
+            if (e.key === "Enter") {
+              e.currentTarget?.blur();
+            }
+          }}
+          contenteditable
+        ></h1>
         <KeyboardList />
       </div>
       <Keyboard />
